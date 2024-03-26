933a266c29da2063dd7a1b0e71b579bde22d58d2
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-03-26 12:47:49 -0400

    add for removing index from array with removeMiddle function

--- staticarrays.py
+++ staticarrays.py
@@ -1,4 +1,19 @@
-myArray = [1,3,5]
+myArray = [1,3,5,7,8,12]
 
-for i in range(len(myArray)):
-    print(myArray[i])
+# for i in range(len(myArray)):
+#     print(myArray[i])
+
+
+
+# removeMiddle(Specify which array, which index to remove, and how many we're removing)
+def removeMiddle(array, i, removal):
+    for index in range(i + removal, len(array)):
+        array[index - removal] = array[index]
+        ##So, if we consider myArray = [1, 3, 5, 7, 8, 12] and the first iteration of the loop:
+        ##Before the assignment: myArray = [1, 3, 5, 7, 8, 12]
+        ##After the assignment: myArray = [1, 5, 5, 7, 8, 12]
+    array = array[:-removal]
+    return array
+result = removeMiddle(myArray, 1, 1)
+print(result)
+
