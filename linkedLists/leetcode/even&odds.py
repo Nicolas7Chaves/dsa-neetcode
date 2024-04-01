5e6d6802f9f2d402ad6bc959ff1cc26d8c334a4b
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-01 11:18:55 -0400

    add leetcode 206 solution and even/odds solution

---
+++ linkedLists/leetcode/even&odds.py
@@ -0,0 +1,13 @@
+array1 = [1,3,4,5,6,8,10]
+
+def evens (nums):
+    for num in nums:
+        if num % 2 != 0:
+            print(num)
+evens(array1)
+
+def evens (nums):
+    for num in nums:
+        if num % 2 == 0:
+            print(num)
+evens(array1)
